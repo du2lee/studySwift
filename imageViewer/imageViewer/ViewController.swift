e1b5ac3804028e4d028609347fb1714c82a3f1b6
duhui <dh151446@gmail.com>
2023-02-13 15:38:29 +0900

    도전미션 : imageView 만들기

--- imageViewer/imageViewer/ViewController.swift
+++ imageViewer/imageViewer/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var page = 1
+    var name: String = ""
 
+    @IBOutlet var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        name = "newJeans_1.jpg"
+        imgView.image = UIImage(named: name)
     }
 
-
+    @IBAction func btnNext(_ sender: UIButton) {
+        if (page < 3){
+            page += 1
+        }
+        name = "newJeans_" + String(page) + ".jpg"
+        imgView.image = UIImage(named: name)
+    }
+    
+    @IBAction func btnPrev(_ sender: UIButton) {
+        if (page > 1){
+            page -= 1
+        }
+        name = "newJeans_" + String(page) + ".jpg"
+        imgView.image = UIImage(named: name)
+    }
 }
 
