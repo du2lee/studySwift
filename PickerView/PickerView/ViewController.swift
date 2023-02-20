a2ea131d48d26b816fbad9902fb11d43229d6de5
duhui <dh151446@gmail.com>
2023-02-20 11:05:42 +0900

    [05]PickerViewInitialize

---
+++ PickerView/PickerView/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PickerView
+//
+//  Created by 이두희 on 2023/02/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var pickImage: UIPickerView!
+    @IBOutlet var lblImageFileName: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
